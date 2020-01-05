d6c918edd26e82fbdb28bb4521e4fcd2beb5c70b
Dom <Domhill1998@gmail.co>
2020-01-05 22:42:54 +0000

    Revert to env connection string

--- MSProductsBackEnd/Startup.cs
+++ MSProductsBackEnd/Startup.cs
@@ -27,13 +27,13 @@ namespace MSProductsBackEnd.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //var conn = Environment.GetEnvironmentVariable("DBConnection");
-            //services.AddDbContext<MSProductsDB>(options => options.UseSqlServer(conn));
+            var conn = Environment.GetEnvironmentVariable("DBConnection");
+            services.AddDbContext<MSProductsDB>(options => options.UseSqlServer(conn));
 
 
             //Test with the live conn string
-            string liveConn = "Server=tcp:msproducts.database.windows.net,1433;Initial Catalog=MSProductsDB;Persist Security Info=False;User ID=s6007824;Password=Rubbertoast2!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            services.AddDbContext<MSProductsDB>(options => options.UseSqlServer(liveConn));
+            //string liveConn = "Server=tcp:msproducts.database.windows.net,1433;Initial Catalog=MSProductsDB;Persist Security Info=False;User ID=s6007824;Password=Rubbertoast2!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //services.AddDbContext<MSProductsDB>(options => options.UseSqlServer(liveConn));
 
 
 
