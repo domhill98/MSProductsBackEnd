1f984798e6ec9f270812810fafc9cc2838ddc8f1
Dom <Domhill1998@gmail.co>
2020-01-06 00:02:15 +0000

    new oirders method

---
+++ MSProductsBackEnd/Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MSProductsBackEnd.Data;
+
+namespace MSProductsBackEnd.API.Controllers
+{
+    [Route("api/Orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly MSProductsDB _context;
+
+        public OrdersController(MSProductsDB context)
+        {
+            _context = context;
+        }
+
+
+
+        [HttpGet("History/{prodID}")]
+        public ActionResult<IEnumerable<OrderHistory>> GetOrderHistory(Guid prodID)
+        {
+            var orders = _context.OrderHistory.Where(x => x.productId == prodID).AsEnumerable();
+
+            return Ok(orders);
+        }
+    }
+}
